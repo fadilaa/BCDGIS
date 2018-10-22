f5a23d36507c72507d2fe1ba54ef15de5b126fc3
Imron Sumadireja <imronsumadireza@gmail.com>
2018-10-22 10:39:50 +0700

    update

--- 1164076_ImronSumadireja/Hari_1/soal1.py
+++ 1164076_ImronSumadireja/Hari_1/soal1.py
@@ -1,14 +1,14 @@
-import shapefile
-w=shapefile.Writer() 
-w.shapeType
+import shapefile  # import modul shapefile
+w = shapefile.Writer()  # deklarasi fungi writer
+w.shapeType  # melakukan apply tipe shape
 
-w.field("kolom1","C")
-w.field("kolom2","C")
+w.field("kolom1", "C")  # membuat kolom dengan tipe character
+w.field("kolom2", "C")
 
-w.record("ngek","satu") 
-w.record("ngok","dua")
+w.record("ngek", "satu")  # isi dari record field diatas
+w.record("ngok", "dua")
 
-w.point(1,1) 
-w.point(2,2)
+w.point(1, 1)  # membuat point/titik
+w.point(2, 2)
 
-w.save("soal1") 
+w.save("soal1")  # untuk menyimpan file.
