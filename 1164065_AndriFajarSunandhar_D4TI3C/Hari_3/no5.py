c2b06bdde8d0c0663f1f34dcaa9cece8391883ed
andriFs <andrirrf7@gmail.com>
2018-10-22 19:06:33 +0700

    update hari 3

---
+++ 1164065_AndriFajarSunandhar_D4TI3C/Hari_3/no5.py
@@ -0,0 +1,15 @@
+import shapefile #Melakukan import modul shapefile
+w=shapefile.Writer() #Mendeklarasikan fungsi writer
+w.shapeType #Apply type shape
+
+w.field("kolom1","C") #Untuk membuat kolom dengan menggunakan tipe data character
+w.field("kolom2","C")
+
+w.record("ngek","satu")#Mengisi record pada kolom yang telah dibuat
+
+
+w.line(parts=[[[1,5],[5,5],[5,1],[3,3],[1,1]]]) #Membuat titik koordinat line dan akan membentuk polyline
+
+w.save("soal5") #menyimpan file dengan format shp
+
+
