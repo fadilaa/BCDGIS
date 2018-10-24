cb4b3a32a32866254f9f40eb659f77cd44fe1b66
andriFs <andrirrf7@gmail.com>
2018-10-24 21:43:51 +0700

    Update

---
+++ 1164065_AndriFajarSunandhar_D4TI3C/Hari_5/no9.py
@@ -0,0 +1,17 @@
+import shapefile        #mengimport modul shapefile
+w=shapefile.Writer()    #mendeklarasikan fungsi writer
+w.shapeType            #meng apply type shape
+
+w.field("kolom1","C")   #membuat kolom dengan menggunakan tipe data character
+w.field("kolom2","C")
+
+w.record("ngek","satu") #mengisi record pada sebuah kolom yang sudah disiapkan
+w.record("crot","dua")
+
+
+
+w.poly(parts=[[[1,3],[5,3], [5,2],[1,2], [1,3]]],shapeType=shapefile.POLYLINE)  #membuat koordinat dan menghasilkan bentuk 1 persegi dan 1 persegi panjang
+w.poly(parts=[[[1,6],[5,6], [5,9],[1,9], [1,6]]],shapeType=shapefile.POLYLINE)
+
+
+w.save("soal9")   #untuk menyimpan file data dengan bentuk shp file
