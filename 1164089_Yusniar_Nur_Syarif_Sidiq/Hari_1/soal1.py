40c8f09775affc43f641659845fd44e544714420
yusniarnss <yusniar33@gmial.com>
2018-10-24 19:22:43 +0700

    Update Hari 5

--- 1164089_Yusniar_Nur_Syarif_Sidiq/Hari_1/soal1.py
+++ 1164089_Yusniar_Nur_Syarif_Sidiq/Hari_1/soal1.py
@@ -1,14 +1,14 @@
-import shapefile
-w=shapefile.Writer()
-w.shapeType
+import shapefile            #mengimport modul shapefile
+w=shapefile.Writer()        #mendeklarasikan fungsi writer
+w.shapeType                 #meng apply type shape
 
-w.field("kolom1","C")
+w.field("kolom1","C")       #membuat kolom dengan menggunakan tipe data character
 w.field("kolom2","C")
 
-w.record("ngek","satu")
+w.record("ngek","satu")     #mengisi record pada sebuah kolom yang sudah disiapkan
 w.record("ngok","dua")
 
-w.point(1,1)
-w.point(2,2)
+w.point(1,1)                #membuat point dengan koordinat 1 pada sumbu x dan 1 pada sumbu y
+w.point(2,2)                #membuat point dengan koordinat 2 pada sumbu x dan 2 pada sumbu y
 
-w.save("soal1")
+w.save("soal1")             #untuk menyimpan file data dengan bentuk shp file
--- 1164089_Yusniar_Nur_Syarif_Sidiq/Hari_2/soal3.py
+++ 1164089_Yusniar_Nur_Syarif_Sidiq/Hari_2/soal3.py
@@ -1,16 +1,16 @@
-import shapefile
-w=shapefile.Writer(shapeType=1)
-w.shapeType
-w.shapeType=3
-w.shapeType
+import shapefile                    #mengimport modul shapefile
+w=shapefile.Writer(shapeType=1)     #mendeklarasikan fungsi writer dan menambahkan parameter shapetype
+w.shapeType                         #meng apply type shape
+w.shapeType=3                       #merubah type shape
+w.shapeType                         #apply type shape
 
-w.field("kolom1","C")
+w.field("kolom1","C")               #membuat kolom dengan menggunakan tipe data character
 w.field("kolom2","C")
 
-w.record("ngek","satu")
+w.record("ngek","satu")             #mengisi record pada sebuah kolom yang sudah disiapkan
 w.record("ngok","dua")
 
-w.point(1,1)
-w.point(2,2)
+w.point(1,1)                        #membuat point dengan koordinat 1 pada sumbu x dan 1 pada sumbu y
+w.point(2,2)                        #membuat point dengan koordinat 2 pada sumbu x dan 2 pada sumbu y
 
-w.save("soal3")
+w.save("soal3")                     #untuk menyimpan file data dengan bentuk shp file
