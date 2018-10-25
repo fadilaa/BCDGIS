21f8d1c4c3453fa7c51d322d1c6c041d8600f491
andriFs <andrirrf7@gmail.com>
2018-10-26 00:47:23 +0700

    Alhamdulilahhh indehoy

--- 1164065_AndriFajarSunandhar_D4TI3C/Hari_6/no10.py
+++ 1164065_AndriFajarSunandhar_D4TI3C/Hari_6/no10.py
@@ -13,12 +13,12 @@ w.record("dudung","Segitiga") #mengisi record pada kolom yang telah disediakan
 w.record("patrik","Segitiga")#mengisi record pada kolom yang telah disediakan
 
 
-w.poly(parts=[[[-1,1],[0,3],[1,1],[-1,1]]],shapeType=shapefile.POLYLINE) #membuat koordinat polygon yang menghasilkan 6 segitiga sama kaki dengan nama sule
-w.poly(parts=[[[1,1],[3,1],[2,3],[1,1]]],shapeType=shapefile.POLYLINE)  #dono
-w.poly(parts=[[[-1,1],[-3,1],[-2,3],[-1,1]]],shapeType=shapefile.POLYLINE) #dadang
-w.poly(parts=[[[-2,3],[-1,5],[0,3],[-2,3]]],shapeType=shapefile.POLYLINE) #diding
-w.poly(parts=[[[2,3],[1,5],[0,3],[2,3]]],shapeType=shapefile.POLYLINE) #dudung
-w.poly(parts=[[[-1,5],[1,5],[0,7],[-1,5]]],shapeType=shapefile.POLYLINE) #patrik
+w.poly(parts=[[[-1.5,1],[0,3.5],[1.5,1],[-1.5,1]]],shapeType=shapefile.POLYLINE) #membuat koordinat polygon yang menghasilkan 6 segitiga sama kaki dengan nama sule
+w.poly(parts=[[[1.5,1],[4.5,1],[3,3.5],[1.5,1]]],shapeType=shapefile.POLYLINE)  #dono
+w.poly(parts=[[[-4.5,1],[-3,3.5],[-1.5,1],[-4.5,1]]],shapeType=shapefile.POLYLINE)  #dadang
+w.poly(parts=[[[-3,3.5],[-1.5,6],[0,3.5],[-3,3.5]]],shapeType=shapefile.POLYLINE)  #diding
+w.poly(parts=[[[3,3.5],[1.5,6],[0,3.5],[3,3.5]]],shapeType=shapefile.POLYLINE)  #dudung
+w.poly(parts=[[[-1.5,6],[0,8.5],[1.5,6],[-1.5,6]]],shapeType=shapefile.POLYLINE) #patrik
 
 
 w.save("soal10")     #untuk menyimpan file data dengan bentuk shp file
